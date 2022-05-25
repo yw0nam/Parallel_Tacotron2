06e449d749647b7856a91b4e73005b21cd5e1082
yw0nam <spow2544@gmail.com>
2022-05-26 02:23:30 +0900

    restore duration loss

--- models/loss.py
+++ models/loss.py
@@ -19,7 +19,7 @@ class ParallelTacotron2Loss(nn.Module):
             gamma=train_config.gamma,
             warp=train_config.warp
         )
-        self.mae_loss = nn.L1Loss()
+        self.mae_loss = nn.L1Loss(reduction='none')
 
     def kl_anneal(self, step):
         if step < self.start:
@@ -45,7 +45,8 @@ class ParallelTacotron2Loss(nn.Module):
         mel_loss = (mel_iter_loss / (len(mel_iters) * mel_length)).mean()
 
         # Duration Loss
-        duration_loss = self.lambda_ * (self.mae_loss(dur.sum(-1), mel_length))
+        duration_loss = self.lambda_ * \
+            (self.mae_loss(dur.sum(-1), mel_length) / text_length).mean()
 
         # KL Divergence Loss
         beta = torch.tensor(self.kl_anneal(step))
